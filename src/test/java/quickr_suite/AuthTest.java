06719a5f3afad6ed0e6f38259e9ef2e067f4f4ea
OmarTheDarkKnight <jayed258@gmail.com>
2020-07-24 01:12:44 -0400

    Add @Before method, constructor and data provider method in AuthTest class

--- src/test/java/quickr_suite/AuthTest.java
+++ src/test/java/quickr_suite/AuthTest.java
@@ -1,4 +1,44 @@
 package quickr_suite;
 
-public class AuthTest {
+import base.BaseTest;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import util.TestUtil;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class AuthTest extends BaseTest {
+    public String userEmail;
+    public String password;
+    public String expectedResult;
+
+    public AuthTest(String userEmail, String password, String expectedResult) {
+        this.userEmail = userEmail;
+        this.password = password;
+        this.expectedResult = expectedResult;
+    }
+
+    @Before
+    public void init() {
+        // check whether to skip this test class or run it
+        if(!TestUtil.runTest("AuthTest"))
+            Assume.assumeTrue(false); // skip the test
+    }
+
+    @Parameters
+    public static Collection<Object[]> getData() {
+        return Arrays.asList(TestUtil.readData("Login"));
+    }
+
+    @Test
+    public void loginTest() {
+
+    }
 }
